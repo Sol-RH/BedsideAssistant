a5b02a049f9eb082bbae96ef828a3a9bfb97120b
Sol-RH <a01747396@tec.mx>
2026-02-17 13:49:47 -0600

    Audio function version 1

---
+++ src/audio/record_audio.py
@@ -0,0 +1,17 @@
+# Captures the voice input 
+
+import sounddevice as sd
+import numpy as np 
+import scipy.io.wavfile as wf
+import queue 
+
+# Mic Configuration 
+rate= 16000
+channels= 1
+buffer= 1024
+
+silence_thresh= 0.01     # considering the hopsital room is mostly silent
+
+
+silence_dur= 2.0    #silence after user stops talking
+min_audio= 0.5      #minium audio duration
